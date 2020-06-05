d70cb4c06299fc81b4eb399e3d4cc022b03b00c1
Sage-ArtZ <48380051+Sage-ArtZ@users.noreply.github.com>
2020-06-05 13:32:45 +0530

    Add files via upload

---
+++ watermelon.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	int in;
+	cin >> in;
+	if (in==2){
+		cout<<"NO";
+	}
+	else{
+		if (in%2==0){
+			cout<<"YES";
+		}
+		else{
+			cout<<"NO";
+		}
+	}
+	return 0;
+}
